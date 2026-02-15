5dfd8fe8f2751cf63a415d071570a58c7d76b481
Aryan Bandooni <aryanbandooni19@gmail.com>
2026-02-15 18:49:23 +0530

    google remove

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.gi6lr9om0vg"
+    "revision": "0.uvsqvbo9a8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
