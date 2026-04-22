29d82630628b15250975f72b9413621137db75d8
Aryan Bandooni <aryanbandooni19@gmail.com>
2026-04-22 21:35:04 +0530

    ui changes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.1ametjcp3do"
+    "revision": "0.3pma95qvlug"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
